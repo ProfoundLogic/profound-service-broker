e8cf0570fc440da92486dce48671d66944d4e917
AaronTheNerd <bargeaaron@gmail.com>
2025-08-27 00:49:49 -0400

    fix(license): made it so different limit types are supported

--- src/services/impl/license-impl.service.ts
+++ src/services/impl/license-impl.service.ts
@@ -6,19 +6,37 @@ import { ServiceInstance } from '../../db/entities/service-instance.entity'
 export class LicenseServiceImpl implements LicenseService {
   private pllsEndpoint = process.env.PLLS_URL || ''
   private pllsApiKey = process.env.PLLS_API_KEY || ''
-  private pllsPUIModules = LicenseServiceImpl.parseModulesEnv(
-    process.env.PLLS_PUI_MODULES || '',
+  private pllsPUIModulesConcurrent = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PUI_MODULES_CONCURRENT || '',
   )
-  private pllsPJSModules = LicenseServiceImpl.parseModulesEnv(
-    process.env.PLLS_PJS_MODULES || '',
+  private pllsPUIModulesNamed = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PUI_MODULES_NAMED || '',
+  )
+  private pllsPUIModulesNoLimit = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PUI_MODULES_NO_LIMIT || '',
+  )
+  private pllsPJSModulesConcurrent = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PJS_MODULES_CONCURRENT || '',
+  )
+  private pllsPJSModulesNamed = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PJS_MODULES_NAMED || '',
+  )
+  private pllsPJSModulesNoLimit = LicenseServiceImpl.parseModulesEnv(
+    process.env.PLLS_PJS_MODULES_NO_LIMIT || '',
   )
 
-  private static readonly API_KEY_HEADER = 'x-plls-api-key'
   private static readonly CUSTOMER_PATH = '/customers/'
   private static readonly LICENSE_PATH = '/licenses/'
   private static readonly AUTHORIZATION_CODE_PATH =
     '/authorization_codes/generate'
   private static readonly LICENSE_SEATS = 50
+
+  private static readonly PERMANENT = 1
+
+  private static readonly NO_LIMIT = 0
+  private static readonly CONCURRENT = 1
+  private static readonly NAMED = 2
+
   private static readonly PJS_PRODUCT_ID = 1
   private static readonly PUI_PRODUCT_ID = 2
 
@@ -29,7 +47,7 @@ export class LicenseServiceImpl implements LicenseService {
     const customerId = await this.createCustomer(customerName)
     let pjsLicenseId = undefined
     let pjsAuthorizationCode = undefined
-    if (this.pllsPJSModules.length > 0) {
+    if (this.productIncludesPJS()) {
       pjsLicenseId = await this.createLicense(
         customerId,
         LicenseServiceImpl.PJS_PRODUCT_ID,
@@ -38,7 +56,7 @@ export class LicenseServiceImpl implements LicenseService {
     }
     let puiLicenseId = undefined
     let puiAuthorizationCode = undefined
-    if (this.pllsPUIModules.length > 0) {
+    if (this.productIncludesPUI()) {
       puiLicenseId = await this.createLicense(
         customerId,
         LicenseServiceImpl.PUI_PRODUCT_ID,
@@ -65,23 +83,47 @@ export class LicenseServiceImpl implements LicenseService {
   }
 
   private static parseModulesEnv(modules_string: string): number[] {
+    if (modules_string.length === 0) return []
     return modules_string.split(',').map(module_string => {
       return parseInt(module_string, 10)
     })
   }
 
+  private productIncludesPJS(): boolean {
+    return (
+      this.pllsPJSModulesConcurrent.length > 0 ||
+      this.pllsPJSModulesNamed.length > 0 ||
+      this.pllsPJSModulesNoLimit.length > 0
+    )
+  }
+
+  private productIncludesPUI(): boolean {
+    return (
+      this.pllsPUIModulesConcurrent.length > 0 ||
+      this.pllsPUIModulesNamed.length > 0 ||
+      this.pllsPUIModulesNoLimit.length > 0
+    )
+  }
+
   private getCustomerName(instanceId: string): string {
-    return `ibm-cloud-${instanceId}`
+    return `IBMCloud${instanceId}`
+  }
+
+  private getHeaders(): any {
+    return {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'x-plls-api-key': this.pllsApiKey,
+    }
   }
 
   private async createCustomer(customerName: string): Promise<number> {
+    const URL = `${this.pllsEndpoint}${LicenseServiceImpl.CUSTOMER_PATH}${customerName}`
     const response = await axios.post(
-      `${this.pllsEndpoint}${LicenseServiceImpl.CUSTOMER_PATH}${customerName}`,
+      URL,
+      {},
       {
-        headers: {
-          Accept: 'application/json',
-          [LicenseServiceImpl.API_KEY_HEADER]: this.pllsApiKey,
-        },
+        headers: this.getHeaders(),
       },
     )
 
@@ -93,15 +135,10 @@ export class LicenseServiceImpl implements LicenseService {
   }
 
   private async getCustomer(customerName: string): Promise<number> {
-    const response = await axios.get(
-      `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${customerName}`,
-      {
-        headers: {
-          Accept: 'application/json',
-          [LicenseServiceImpl.API_KEY_HEADER]: this.pllsApiKey,
-        },
-      },
-    )
+    const URL = `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${customerName}`
+    const response = await axios.get(URL, {
+      headers: this.getHeaders(),
+    })
 
     if (response.data && typeof response.data === 'object') {
       return response.data.customer_id
@@ -114,14 +151,12 @@ export class LicenseServiceImpl implements LicenseService {
     customerId: number,
     product_id: number,
   ): Promise<number> {
+    const URL = `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${customerId}`
     const response = await axios.post(
-      `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${customerId}`,
+      URL,
       this.buildLicensePayload(LicenseServiceImpl.LICENSE_SEATS, product_id),
       {
-        headers: {
-          Accept: 'application/json',
-          [LicenseServiceImpl.API_KEY_HEADER]: this.pllsApiKey,
-        },
+        headers: this.getHeaders(),
       },
     )
 
@@ -133,10 +168,18 @@ export class LicenseServiceImpl implements LicenseService {
   }
 
   private buildLicensePayload(licenseSeats: number, product_id: number) {
-    const modules_env =
+    const concurrent_modules =
       product_id === LicenseServiceImpl.PJS_PRODUCT_ID
-        ? this.pllsPJSModules
-        : this.pllsPUIModules
+        ? this.pllsPJSModulesConcurrent
+        : this.pllsPUIModulesConcurrent
+    const named_modules =
+      product_id === LicenseServiceImpl.PJS_PRODUCT_ID
+        ? this.pllsPJSModulesNamed
+        : this.pllsPUIModulesNamed
+    const no_limit_modules =
+      product_id === LicenseServiceImpl.PJS_PRODUCT_ID
+        ? this.pllsPJSModulesNoLimit
+        : this.pllsPUIModulesNoLimit
     return {
       license_type: 2,
       floating: {
@@ -145,40 +188,51 @@ export class LicenseServiceImpl implements LicenseService {
       },
       product_id,
       description: 'IBM Cloud License Key',
-      modules: modules_env.map(pllsModule => {
-        return {
-          module_id: pllsModule,
-          term_id: 1,
-          limit_id: 1,
-          usage_limit: licenseSeats,
-        }
-      }),
+      modules: [
+        ...concurrent_modules.map(pllsModule => {
+          return {
+            module_id: pllsModule,
+            term_id: LicenseServiceImpl.PERMANENT,
+            limit_id: LicenseServiceImpl.CONCURRENT,
+            usage_limit: licenseSeats,
+          }
+        }),
+        ...named_modules.map(pllsModule => {
+          return {
+            module_id: pllsModule,
+            term_id: LicenseServiceImpl.PERMANENT,
+            limit_id: LicenseServiceImpl.NAMED,
+            usage_limit: licenseSeats,
+          }
+        }),
+        ...no_limit_modules.map(pllsModule => {
+          return {
+            module_id: pllsModule,
+            term_id: LicenseServiceImpl.PERMANENT,
+            limit_id: LicenseServiceImpl.NO_LIMIT,
+          }
+        }),
+      ],
     }
   }
 
   private async deleteLicense(licenseId: number) {
-    await axios.delete(
-      `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${licenseId}`,
-      {
-        headers: {
-          Accept: 'application/json',
-          [LicenseServiceImpl.API_KEY_HEADER]: this.pllsApiKey,
-        },
-      },
-    )
+    const URL = `${this.pllsEndpoint}${LicenseServiceImpl.LICENSE_PATH}${licenseId}`
+    await axios.delete(URL, {
+      headers: this.getHeaders(),
+    })
   }
 
   private async createAuthorizationCode(licenseId: number): Promise<string> {
+    const URL = `${this.pllsEndpoint}${LicenseServiceImpl.AUTHORIZATION_CODE_PATH}`
     const response = await axios.post(
-      `${this.pllsEndpoint}${LicenseServiceImpl.AUTHORIZATION_CODE_PATH}`,
+      URL,
       {
         license_id: licenseId,
+        expires: this.getExpiresString(),
       },
       {
-        headers: {
-          Accept: 'application/json',
-          [LicenseServiceImpl.API_KEY_HEADER]: this.pllsApiKey,
-        },
+        headers: this.getHeaders(),
       },
     )
 
@@ -187,5 +241,12 @@ export class LicenseServiceImpl implements LicenseService {
     } else {
       throw new Error('Failed to generate Authorization Code')
     }
+  }
+
+  private getExpiresString(): string {
+    const expiresDate = new Date()
+    expiresDate.setFullYear(expiresDate.getFullYear() + 5)
+    const expiresISO = expiresDate.toISOString()
+    return expiresISO.split('T')[0]
   }
 }
